45c099b2570bf307821dc7721d679dccf8bbe55b
Michael Currier <mikecurrier18@gmail.com>
2023-05-15 19:09:38 -0400

    removed alt redundant call

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ return (
   <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
       <Link  to="home" smooth={true}  duration={500} >
-          <img src={Logo} alt="Logo Image" style={{height: '50px'}}/>
+          <img src={Logo} alt="Logo" style={{height: '50px'}}/>
        </Link>           
       </div>  
 
